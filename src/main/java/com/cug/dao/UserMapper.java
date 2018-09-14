3869992cd9258679c1597ebfd55f08d342520570
wf <609425241@qq.com>
2018-09-14 11:30:45 +0800

    project init commit

---
+++ src/main/java/com/cug/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.cug.dao;
+
+import com.cug.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
